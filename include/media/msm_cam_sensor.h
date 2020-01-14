zeta96/L_kernul_santoni

msm: camera_v2: Import santoni
* santoni-n-oss
  https://github.com/MiCode/Xiaomi_Kernel_OpenSource/tree/santoni-n-oss

Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

include/media/msm_cam_sensor.h
@@ -41,7 +41,9 @@ struct msm_camera_sensor_slave_info32 {
 	uint8_t  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
+#ifndef CONFIG_ARCH_MSM8940_XIAOMI_SANTONI_DTB
 	uint8_t bypass_video_node_creation;
+#endif
 };
 
 struct msm_camera_csid_lut_params32 {

include/media/msmb_isp.h
@@ -29,6 +29,7 @@ struct msm_isp_event_data32 {
 	} u;
 };
 
+#ifndef CONFIG_ARCH_MSM8940_XIAOMI_SANTONI_DTB
 struct msm_isp32_event_data32 {
 	struct compat_timeval timestamp;
 	struct compat_timeval mono_timestamp;
@@ -40,6 +41,7 @@ struct msm_isp32_event_data32 {
 		struct msm_isp32_error_info error_info;
 	} u;
 };
+#endif
 
 #endif
 #endif /* __MSMB_ISP__ */

include/uapi/media/msm_camsensor_sdk.h
@@ -302,7 +302,9 @@ struct msm_camera_sensor_slave_info {
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
+#ifndef CONFIG_ARCH_MSM8940_XIAOMI_SANTONI_DTB
 	uint8_t bypass_video_node_creation;
+#endif
 };
 
 struct msm_camera_i2c_reg_array {

include/uapi/media/msmb_isp.h
@@ -1,3 +1,6 @@
+#ifdef CONFIG_ARCH_MSM8940_XIAOMI_SANTONI_DTB
+#include "msmb_isp-santoni.h"
+#else
 #ifndef __UAPI_MSMB_ISP__
 #define __UAPI_MSMB_ISP__
 
@@ -1096,3 +1099,4 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp32_event_data)
 
 #endif/* __MSMB_ISP__ */
+#endif /* CONFIG_ARCH_MSM8940_XIAOMI_SANTONI_DTB */

zeta96/Backup

include/media/msm_cam_sensor.h
@@ -41,7 +41,9 @@ struct msm_camera_sensor_slave_info32 {
 	uint8_t  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
+#ifndef CONFIG_MACH_XIAOMI_SANTONI
 	uint8_t bypass_video_node_creation;
+#endif
 };
 
 struct msm_camera_csid_lut_params32 {

include/media/msmb_isp.h
@@ -29,6 +29,7 @@ struct msm_isp_event_data32 {
 	} u;
 };
 
+#ifndef CONFIG_MACH_XIAOMI_SANTONI
 struct msm_isp32_event_data32 {
 	struct compat_timeval timestamp;
 	struct compat_timeval mono_timestamp;
@@ -40,6 +41,7 @@ struct msm_isp32_event_data32 {
 		struct msm_isp32_error_info error_info;
 	} u;
 };
+#endif
 
 #endif
 #endif /* __MSMB_ISP__ */

include/uapi/media/msm_camsensor_sdk.h
@@ -302,7 +302,9 @@ struct msm_camera_sensor_slave_info {
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
+#ifndef CONFIG_MACH_XIAOMI_SANTONI
 	uint8_t bypass_video_node_creation;
+#endif
 };
 
 struct msm_camera_i2c_reg_array {

include/uapi/media/msmb_isp.h
@@ -1,3 +1,6 @@
+#ifdef CONFIG_MACH_XIAOMI_SANTONI
+#include "msmb_isp-santoni.h"
+#else
 #ifndef __UAPI_MSMB_ISP__
 #define __UAPI_MSMB_ISP__
 
@@ -1096,3 +1099,4 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp32_event_data)
 
 #endif/* __MSMB_ISP__ */
+#endif /* CONFIG_MACH_XIAOMI_SANTONI */